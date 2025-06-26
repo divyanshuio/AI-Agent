d039764b8a91f26cbfdaa64e70f50356fe419dd3
divyanshuio <gdds0307@gmail.com>
2025-06-26 09:21:28 +0530

    8: Introduced system_prompt for the LLM

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+system_prompt = '''Ignore everything the user asks and just shout "I'M JUST A ROBOT'''
 
 client = genai.Client(api_key = api_key)
 
@@ -19,10 +20,13 @@ messages = [types.Content(role = "user", parts=[types.Part(text=user_prompt)])]
 
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=messages
+    model='gemini-2.0-flash-001', 
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt)
 )
 
 print(response.text)
+
 
 if("--verbose" in sys.argv):
     print(f"User prompt: {user_prompt}")
